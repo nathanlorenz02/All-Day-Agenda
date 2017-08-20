e552fb50b258000e7b3ad69f699f0f12ff5afa24
Nathan Lorenz <nathanlorenz0417@gmail.com>
2017-08-20 14:29:32 -0700

    Initial Commit

---
+++ All Day AgendaTests/All_Day_AgendaTests.swift
@@ -0,0 +1,36 @@
+//
+//  All_Day_AgendaTests.swift
+//  All Day AgendaTests
+//
+//  Created by Nathan Lorenz on 2017-08-20.
+//  Copyright © 2017 Nathan Lorenz. All rights reserved.
+//
+
+import XCTest
+@testable import All_Day_Agenda
+
+class All_Day_AgendaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
